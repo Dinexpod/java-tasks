55d56589adf5883683444b718e44921ec373e7ca
dinexpod <work.toxa@gmail.com>
2021-03-30 22:22:43 +0300

    added tasks

---
+++ src/lesson1/Task6.java
@@ -0,0 +1,15 @@
+package lesson1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        //Не думать о секундах…
+        //
+        //Реализуй метод getCountSecondsPassedInCurrentHour(int seconds). Метод на вход принимает целое число секунд,
+        //которые прошли с начала текущих суток. Нужно найти количество секунд, прошедших с начала последнего часа, и
+        //вернуть это число.
+        //Пример:
+        //Метод getCountSecondsPassedInCurrentHour вызывается с параметром 12345.
+        //Пример вывода:
+        //1545
+    }
+}
