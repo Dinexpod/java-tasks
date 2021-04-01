ce4d082b41f7fdfcb776a18793686cd1719c2a75
dinexpod <work.toxa@gmail.com>
2021-04-01 23:09:28 +0300

    added lesson 2 Task1

---
+++ src/lesson2/Task1.java
@@ -0,0 +1,9 @@
+package lesson2;
+
+public class Task1 {
+    public static void main(String[] args) {
+        //Немедленно в печать
+        //
+        //Добавь метод public static void printString(String s), который будет выводить переданную строку на экран.
+    }
+}
