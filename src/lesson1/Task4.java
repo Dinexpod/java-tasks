8cbc1e1deb16195d27401ac572cc64237c6e31ce
dinexpod <work.toxa@gmail.com>
2021-04-02 12:35:17 +0300

    fix tasks: 4, 5, 6, 7

--- src/lesson1/Task4.java
+++ src/lesson1/Task4.java
@@ -2,13 +2,20 @@ package lesson1;
 
 public class Task4 {
     public static void main(String[] args) {
-        //Полнометражная картина
-        //
-        //Реализуй метод getMetreFromCentimetre(int centimetre). Метод на вход принимает количество сантиметров. Нужно сделать так,
-        //чтобы метод возвращал число полных метров в centimetre. (1 метр = 100 см).
-        //Пример:
-        //Метод getMetreFromCentimetre вызывается с параметром 243.
-        //Пример вывода:
-        //2
+        getMetreFromCentimetre(234);
+        getMetreFromCentimetre(155);
+        getMetreFromCentimetre(865);
+    }
+
+    //Полнометражная картина
+    //
+    //Реализуй метод getMetreFromCentimetre(int centimetre). Метод на вход принимает количество сантиметров.
+    // Нужно сделать так, чтобы метод выводил число полных метров в centimetre. (1 метр = 100 см).
+    //Пример:
+    //Метод getMetreFromCentimetre вызывается с параметром 243.
+    //Пример вывода:
+    //2
+    static void getMetreFromCentimetre(int centimetre) {
+
     }
 }
--- src/lesson1/Task6.java
+++ src/lesson1/Task6.java
@@ -2,14 +2,19 @@ package lesson1;
 
 public class Task6 {
     public static void main(String[] args) {
-        //Не думать о секундах…
-        //
-        //Реализуй метод getCountSecondsPassedInCurrentHour(int seconds). Метод на вход принимает целое число секунд,
-        //которые прошли с начала текущих суток. Нужно найти количество секунд, прошедших с начала последнего часа, и
-        //вернуть это число.
-        //Пример:
-        //Метод getCountSecondsPassedInCurrentHour вызывается с параметром 12345.
-        //Пример вывода:
-        //1545
+        getCountSecondsPassedInCurrentHour(12345);
+    }
+
+    //Не думать о секундах…
+    //
+    //Реализуй метод getCountSecondsPassedInCurrentHour(int seconds). Метод на вход принимает целое число секунд,
+    //которые прошли с начала текущих суток. Нужно найти количество секунд, прошедших с начала последнего часа, и
+    //вывести это число.
+    //Пример:
+    //Метод getCountSecondsPassedInCurrentHour вызывается с параметром 12345.
+    //Пример вывода:
+    //1545
+    static void getCountSecondsPassedInCurrentHour(int seconds) {
+
     }
 }
